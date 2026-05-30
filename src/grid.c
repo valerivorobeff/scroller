2dded48ebdb4e624d7490dcb6ffe9f45d9e6cd2c
valerivorobeff <valeri.vorobeff@gmail.com>
2026-05-30 22:46:30 +0300

    1-grid Adding new functions

--- src/grid.c
+++ src/grid.c
@@ -21,45 +21,75 @@ grid_init(Page page, uint16_t pagesz, GridType type, uint16_t rowsz) {
 }
 
 Row
-grid_get_row(Grid *grid, uint16_t i) {
-    return grid->datum + grid->rowsz * i;
+grid_get_row(Grid *grid, uint16_t n) {
+    return grid->datum + grid->rowsz * n;
 }
 
-typedef struct HColumn {
-    char name[16];
-    size_t offs;
-    size_t size;
-} HColumn;
+Row
+grid_alloc_row(Grid *grid) {
+    if (grid->occupied < grid->rown)
+        return grid_get_row(grid, grid->occupied++);
+    else
+        return NULL;
+}
+
+HColumn *
+hgrid_add_column(Grid *grid, const char *name, size_t size) {
+    HColumn *hc = grid_alloc_row(grid);
+
+    if (hc) {
+        strncpy(hc->name, name, NAMESZ);
+        hc->size = size;
+
+        if (grid->occupied > 1)
+            hc->offs = (hc - 1)->offs + size;
+        else
+            hc->offs = 0;
+
+        return hc;
+    } else
+        return NULL;
+}
+
+size_t
+hgrid_get_row_size(Grid *grid) {
+    if (grid->occupied) {
+        HColumn *hc = grid_get_row(grid, grid->occupied - 1);
+        return hc->offs + hc->size;
+    } else
+       return 0;
+}
 
 int
 main() {
-    HColumn hcolumn;
     Page *hp = malloc(8096), *p = malloc(8096);
     Grid *hg = grid_init(hp, 8096, GT_FIXED, sizeof(HColumn)), *g;
     Row row;
 
     /* CREATE TABLE HEADER */
-    /* ALTER TABLE ADD COLUMN 0 */
-    strcpy(hcolumn.name, "column 0");
-    hcolumn.size = 4;
+    /* ALTER TABLE ADD COLUMN */
+    if (!hgrid_add_column(hg, "id_user", 4)) {
 
-    row = grid_get_row(hg, 0);
-    memcpy(row, &hcolumn, sizeof(HColumn));
+    }
+    /* ALTER TABLE ADD COLUMN */
+    if (!hgrid_add_column(hg, "name", 12)) {
 
-    /* ALTER TABLE ADD COLUMN 1 */
-    strcpy(hcolumn.name, "column 1");
-    hcolumn.size = 12;
-
-    row = grid_get_row(hg, 1);
-    memcpy(row, &hcolumn, sizeof(HColumn));
+    }
 
     /* CREATE TABLE DATA */
-    g = grid_init(p, 8096, GT_FIXED, 16);
+    g = grid_init(p, 8096, GT_FIXED, hgrid_get_row_size(hg));
 
-    /* INSERT */
-    row = grid_get_row(g, 0);
-    strcpy(row + 0, "123");
-    strcpy(row + 4, "456789");
+    /* INSERT ROW INTO TABLE */
+    row = grid_alloc_row(g);    /* alloc row */
+    if (row) {
+        HColumn *hc = grid_get_row(hg, 0);
+        Column c = row + hc->offs;
+        strncpy(c, "123", hc->size);
+
+        hc = grid_get_row(hg, 1);
+        c = row + hc->offs;
+        strncpy(c, "12345678", hc->size);
+    }
 
     return 0;
 }
--- src/grid.h
+++ src/grid.h
@@ -2,9 +2,11 @@
 #define _GRID_H_
 
 #include <stdint.h>
+#include <stddef.h>
 
 typedef void *Page;
 typedef void *Row;
+typedef void *Column;
 
 typedef enum GridType : uint16_t {
     GT_FIXED = 1
@@ -16,12 +18,24 @@ typedef struct Grid {
     GridType    type;
     uint16_t    rowsz;
     uint16_t    rown;
-    char        reserved[4];
+    uint16_t    occupied;
+    char        reserved[2];
     char        datum[];
 } Grid;
 
+#define NAMESZ  16
+
+typedef struct HColumn {
+    char name[NAMESZ];
+    size_t offs;
+    size_t size;
+} HColumn;
+
 Grid *grid_init(Page page, uint16_t pagesz, GridType type, uint16_t rowsz);
-Row grid_get_row(Grid *grid, uint16_t i);
+Row grid_get_row(Grid *grid, uint16_t n);
+Row grid_alloc_row(Grid *grid);
+HColumn *hgrid_add_column(Grid *grid, const char *name, size_t size);
+size_t hgrid_get_row_size(Grid *grid);
 
 #endif /* _GRID_H_ */
 
